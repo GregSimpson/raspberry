84f2a637c1a87b654bc53da3208c57750da1b511
GregSimpson <gsimpson@pobox.com>
2017-07-28 12:44:49 -0600

    motion working at bootup

--- motion/bouvet/runMotion.sh
+++ motion/bouvet/runMotion.sh
@@ -11,8 +11,8 @@
 
 ####  camera still shot test
 ## https://www.raspberrypi.org/documentation/usage/camera/raspicam/README.md
-DATE=$(date +"%Y-%m-%d_%H%M")
-raspistill -vf -hf -o /home/pi/camera/bouvet/images/$DATE.jpg
+#DATE=$(date +"%Y-%m-%d_%H%M")
+#raspistill -vf -hf -o /home/pi/raspberry/motion/bouvet/images/$DATE.jpg
 
 # https://www.bouvet.no/bouvet-deler/utbrudd/building-a-motion-activated-security-camera-with-the-raspberry-pi-zero
 
@@ -28,16 +28,23 @@ raspistill -vf -hf -o /home/pi/camera/bouvet/images/$DATE.jpg
 # gvim ~/.motion/motion.conf
 #  Remember that you always have a clean copy of the config file at  /etc/motion/motion.conf.
 
-## !!! gjs
-# https://raspberrypi.stackexchange.com/questions/10480/raspi-camera-board-and-motion/22924
-sudo modprobe bcm2835-v4l2
-
-# fails with /dev/video0 : No such file or directory
-motion -c ~/.motion/motion.conf
-
 # https://www.raspberrypi.org/forums/viewtopic.php?t=76558&p=546467
 # sudo rpi-update
 
+## !!! gjs
+# fails with /dev/video0 : No such file or directory
+# answer:
+# https://raspberrypi.stackexchange.com/questions/10480/raspi-camera-board-and-motion/22924
+#sudo modprobe bcm2835-v4l2
 
+# http://www.richardmudhar.com/blog/2015/02/raspberry-pi-camera-and-motion-out-of-the-box-sparrowcam/
+# to load modprobe at boot time add this line to
+# /etc/modules
+#   # camera with v4l2 driver
+#   bcm2835-v4l2
 
+touch /home/pi/runMotion-TRIED-to-start
+#sleep 10
+motion -c /home/pi/.motion/motion.conf
+touch /home/pi/runMotion-PASSED-to-start
 
